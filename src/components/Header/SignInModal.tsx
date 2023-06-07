82eaec6c398e82c01af68d468527550b5934db07
Jack Agtual <jaagtual@gmail.com>
2023-06-06 22:40:49 -0700

    Allowing user to close sign in modal

--- src/components/Header/SignInModal.tsx
+++ src/components/Header/SignInModal.tsx
@@ -1,5 +1,18 @@
-import { Modal, Box } from '@mui/material'
+import { Modal, Box, IconButton } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
 import UserSelection from './UserSelection'
+
+const modalStyle = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '3px',
+}
 
 type SignInModalProps = {
   signInModalOpen: boolean
@@ -12,21 +25,19 @@ function SignInModal({
   setUsername,
   setSignInModalOpen,
 }: SignInModalProps) {
-  const modalStyle = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    boxShadow: 24,
-    p: 4,
-    borderRadius: '3px',
+  const handleCloseModal = () => {
+    setSignInModalOpen(false)
+    setUsername('')
   }
 
   return (
     <Modal open={signInModalOpen}>
       <Box sx={modalStyle}>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <IconButton onClick={handleCloseModal}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
         <UserSelection
           setUsername={setUsername}
           setSignInModalOpen={setSignInModalOpen}
