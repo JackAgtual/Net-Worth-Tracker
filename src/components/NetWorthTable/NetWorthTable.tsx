51705e155ff328aa244e7d3acc27340cb19bf9b5
Jack Agtual <jaagtual@gmail.com>
2023-06-08 19:37:43 -0700

    Able to sort table by date or net worth

--- src/components/NetWorthTable/NetWorthTable.tsx
+++ src/components/NetWorthTable/NetWorthTable.tsx
@@ -14,16 +14,28 @@ import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 import { Record } from '../../types/data'
 import NetWorthTableRow from './NetWorthTableRow'
 
+const sortButtonShownStyle = {}
+const sortButtonHiddenStyle = { opacity: 0, '&:hover': { opacity: 1 } }
+
+type SortOptions = 'date' | 'netWorth'
+
 type NetWorthTableProps = {
   records: Record[]
   setRecords: React.Dispatch<React.SetStateAction<Record[]>>
 }
 
 function NetWorthTable({ records, setRecords }: NetWorthTableProps) {
-  const [sortByDateOrNetWorth, setSortByDateOrNetWorth] = useState<'date' | 'netWorth'>(
-    'date'
-  )
+  const [sortByDateOrNetWorth, setSortByDateOrNetWorth] = useState<SortOptions>('date')
   const [sortAscending, setSortAscending] = useState(true)
+
+  const handleSortChange = (sortBy: SortOptions) => {
+    if (sortBy === sortByDateOrNetWorth) {
+      setSortAscending((prevSortAscending) => !prevSortAscending)
+    } else {
+      setSortByDateOrNetWorth(sortBy)
+      setSortAscending(true)
+    }
+  }
 
   useEffect(() => {
     console.log({ sortByDateOrNetWorth, sortAscending, records })
@@ -52,8 +64,19 @@ function NetWorthTable({ records, setRecords }: NetWorthTableProps) {
               <Box
                 sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}
               >
-                <IconButton>
-                  <ArrowUpwardIcon />
+                <IconButton
+                  onClick={() => handleSortChange('date')}
+                  sx={
+                    sortByDateOrNetWorth === 'date'
+                      ? sortButtonShownStyle
+                      : sortButtonHiddenStyle
+                  }
+                >
+                  {sortByDateOrNetWorth !== 'date' || sortAscending ? (
+                    <ArrowUpwardIcon />
+                  ) : (
+                    <ArrowDownwardIcon />
+                  )}
                 </IconButton>
                 <Typography variant="subtitle1" component="h6">
                   Date
@@ -64,8 +87,19 @@ function NetWorthTable({ records, setRecords }: NetWorthTableProps) {
               <Box
                 sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}
               >
-                <IconButton>
-                  <ArrowDownwardIcon />
+                <IconButton
+                  onClick={() => handleSortChange('netWorth')}
+                  sx={
+                    sortByDateOrNetWorth === 'netWorth'
+                      ? sortButtonShownStyle
+                      : sortButtonHiddenStyle
+                  }
+                >
+                  {sortByDateOrNetWorth !== 'netWorth' || sortAscending ? (
+                    <ArrowUpwardIcon />
+                  ) : (
+                    <ArrowDownwardIcon />
+                  )}
                 </IconButton>
                 <Typography variant="subtitle1" component="h6">
                   Net Worth
