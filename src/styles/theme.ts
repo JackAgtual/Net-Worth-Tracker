144b941e6c851b9fa67a9712a3103c71db1b86cd
Jack Agtual <jaagtual@gmail.com>
2023-10-20 21:51:22 -0700

    Fixing import error

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material'
-import { teal } from '@mui/material/colors'
+import teal from '@mui/material/colors/teal'
 
 export const theme = createTheme({
   palette: {
